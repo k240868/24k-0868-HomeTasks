df20bc16614d24e338120f9f68856c5ca28e1cfa
k240868 <k240868@nu.edu.pk>
2025-09-19 03:46:39 -0700

    Q4.cpp

---
+++ 24k-0868-LAB-4/Q4.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int interpolationSearch(int arr[], int size, int target) {
+    int low = 0, high = size - 1;
+
+    while (low <= high && target >= arr[low] && target <= arr[high]) {
+        int pos = low + ((target - arr[low]) * (high - low)) / (arr[high] - arr[low]);
+
+        if (arr[pos] == target) {
+            return pos;
+        } else if (arr[pos] < target) {
+            low = pos + 1;
+        } else {
+            high = pos - 1;
+        }
+    }
+
+    return -1;
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int size, target;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+
+    insertionSort(arr, size);
+
+    cout << "\nSorted Array: ";
+    printArray(arr, size);
+
+    cout << "Enter the target element to search for: ";
+    cin >> target;
+
+    int result = interpolationSearch(arr, size, target);
+
+    if (result != -1) {
+        cout << "Element " << target << " found at index " << result << endl;
+    } else {
+        cout << "Element " << target << " not found in the array." << endl;
+    }
+
+    return 0;
+}
