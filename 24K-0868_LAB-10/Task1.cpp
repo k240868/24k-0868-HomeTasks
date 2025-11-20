06b07b7d8b83b3eb633695997436a1bab3ef4a45
k240868 <k240868@nu.edu.pk>
2025-11-20 22:41:05 +0500

    Task1.cpp

---
+++ 24K-0868_LAB-10/Task1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void insertHeap(int h[], int &n, int x) {
+    h[n] = x;
+    int i = n;
+    n++;
+    while (i > 0) {
+        int p = (i - 1) / 2;
+        if (h[p] > h[i]) {
+            int t = h[p];
+            h[p] = h[i];
+            h[i] = t;
+            i = p;
+        } else break;
+    }
+}
+
+int main() {
+    int h[20];
+    int n = 5;
+
+    h[0] = 1;
+    h[1] = 7;
+    h[2] = 9;
+    h[3] = 0;
+    h[4] = 6;
+
+    insertHeap(h, n, 2);
+
+    for (int i = 0; i < n; i++) cout << h[i] << " ";
+}
